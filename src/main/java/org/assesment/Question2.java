75f984ce7d69c96b5130533679ad840a290cddb5
Dimitris Mittas <jimacos1996@hotmail.com>
2025-02-16 16:52:38 +0200

    Optimization , object initialization

--- src/main/java/org/assesment/Question2.java
+++ src/main/java/org/assesment/Question2.java
@@ -10,7 +10,6 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
-
 import java.time.Duration;
 import java.util.List;
 import java.util.Random;
@@ -18,49 +17,58 @@ import java.util.Random;
 public class Question2 {
 
     private static WebDriver driver;
-
     private static WebDriverWait wait;
-
-    Random random = new Random();
-
-    ChromeOptions options = new ChromeOptions();
+    private Random random;
+    private ChromeOptions options;
 
     @Before
     public void setup() {
         // Automatically download and set up ChromeDriver
         WebDriverManager.chromedriver().setup();
+
         // Set up Chrome options to enable incognito mode
+        options = new ChromeOptions();
         options.addArguments("--incognito");  // Launch Chrome in Incognito mode
+
         // Initialize ChromeDriver with the options
         driver = new ChromeDriver(options);
         driver.manage().window().maximize();
+
+        // Initialize WebDriverWait and Random
+        wait = new WebDriverWait(driver, Duration.ofSeconds(3));
+        random = new Random();
     }
 
     @Test
     public void testAssessment() {
         driver.get("file:///C:/Users/Jim/AppData/Local/Temp/%7B3ED5BD84-4ACD-44FD-A750-4DBCFA8E0D60%7D/%7BC3250C1F-B661-4528-8E92-7975B136173E%7D/QuestionnariePage.xml");
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3));
+
+        // Wait for element visibility
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("j_idt39:j_idt43")));
         Assert.assertEquals("Under Appraisal / Operation Active", driver.findElement(By.id("j_idt39:j_idt43")).getText());
-        //Create a list that contains all radio buttons
+
+        // Create a list that contains all radio buttons
         List<WebElement> radioButtons = driver.findElements(By.xpath("//*[local-name()='input' and @type='radio' and (@value='Y' or @value='N')]"));
 
+        // Randomly select either "Yes" or "No" for each question
         for (WebElement radioButton : radioButtons) {
-            if (random.nextBoolean()) {
-                if (radioButton.getAttribute("value").equals("Y") && !radioButton.isSelected()) {
+            // Check if the radio button is not selected
+            if (!radioButton.isSelected()) {
+                // Randomly click either Yes or No
+                if (random.nextBoolean() && radioButton.getAttribute("value").equals("Y")) {
                     radioButton.click();
-                }
-            } else {
-                if (radioButton.getAttribute("value").equals("N") && !radioButton.isSelected()) {
+                } else if (!random.nextBoolean() && radioButton.getAttribute("value").equals("N")) {
                     radioButton.click();
                 }
             }
         }
-
+        System.out.println("All radio buttons have been assigned option");
     }
 
     @After
     public void tearDown() {
+        // Delete cookies and quit the driver
+        driver.manage().deleteAllCookies();
         driver.quit();
     }
 }
